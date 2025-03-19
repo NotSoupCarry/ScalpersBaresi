d4a1337d30320d428cee08121520eac20ff8e289
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-19 17:28:22 +0100

    zzz

--- home/home.js
+++ home/home.js
@@ -3,18 +3,37 @@ const npcStarter = document.getElementById("npcStarter");
 const npcDialog = document.getElementById("npcDialog");
 const npcDialogText = document.getElementById("npcDialogText");
 const closeNpcDialog = document.getElementById("closeNpcDialog");
+const sleepMenu = document.getElementById("sleepMenu");
+const sleepStarter = document.getElementById("sleepStarter");
+const sleepText = document.getElementById("sleepText");
 // Funzione per avviare il dialogo
 function startDialog() {
     npcDialogText.textContent = "si proprj nu trmon";
-    npcDialog.style.display = "block";
+    npcDialog.style.display = "inline-block";
     npcStarter.style.display = "none";
-    closeNpcDialog.style.display = "block";
+    closeNpcDialog.style.display = "inline-block";
+    sleepMenu.style.display = "none";
 }
 // Funzione per chiudere il dialogo
 function closeDialog() {
-    npcStarter.style.display = "block";
+    npcStarter.style.display = "inline-block";
+    sleepMenu.style.display = "inline-block";
     npcDialog.style.display = "none";
+}
+// Funzione per dormire
+function sleep() {
+    npcStarter.style.display = "none";
+    npcDialog.style.display = "none";
+    sleepStarter.style.display = "none";
+    sleepText.textContent = "zzz";
+    setTimeout(() => {
+        sleepText.textContent = "HAI RIPOSATO";
+        setTimeout(() => {
+            location.reload();
+        }, 2000);
+    }, 5000);
 }
 // Aggiungi event listener ai bottoni
 npcStarter.addEventListener("click", startDialog);
 closeNpcDialog.addEventListener("click", closeDialog);
+sleepStarter.addEventListener("click", sleep); // Aggiungi evento per il bottone "Dormi"
--- home/home.ts
+++ home/home.ts
@@ -1,24 +1,45 @@
 const npcStarter =  document.getElementById("npcStarter") as HTMLButtonElement;
-
 const npcDialog = document.getElementById("npcDialog") as HTMLDivElement;
-const npcDialogText = document.getElementById("npcDialogText") as HTMLParagraphElement
+const npcDialogText = document.getElementById("npcDialogText") as HTMLParagraphElement;
 const closeNpcDialog = document.getElementById("closeNpcDialog") as HTMLButtonElement;
+const sleepMenu = document.getElementById("sleepMenu") as HTMLDivElement;
+const sleepStarter = document.getElementById("sleepStarter") as HTMLButtonElement;
+const sleepText = document.getElementById("sleepText") as HTMLParagraphElement;
+const return_index = document.getElementById("return_index") as HTMLAnchorElement;
 
-
-// Funzione per avviare il dialogo
+// Funzione per avviare il dialogo 
 function startDialog() {
     npcDialogText.textContent = "si proprj nu trmon";
-    npcDialog.style.display = "block";
+    npcDialog.style.display = "inline-block";
     npcStarter.style.display = "none";
-    closeNpcDialog.style.display = "block";
+    closeNpcDialog.style.display = "inline-block";
+    sleepMenu.style.display = "none";
 }
 
 // Funzione per chiudere il dialogo
 function closeDialog() {
-    npcStarter.style.display = "block";
+    npcStarter.style.display = "inline-block";
+    sleepMenu.style.display = "inline-block";
     npcDialog.style.display = "none"; 
+}
+
+// Funzione per dormire
+function sleep() {
+    npcStarter.style.display = "none";
+    npcDialog.style.display = "none";
+    sleepStarter.style.display = "none";
+    return_index.style.display = "none";
+    sleepText.textContent = "zzz"; 
+    
+    setTimeout(() => {
+        sleepText.textContent = "HAI RIPOSATO"; 
+        setTimeout(() => {
+            location.reload(); 
+        }, 1000);
+    }, 5000);
 }
 
 // Aggiungi event listener ai bottoni
 npcStarter.addEventListener("click", startDialog);
 closeNpcDialog.addEventListener("click", closeDialog);
+sleepStarter.addEventListener("click", sleep); // Aggiungi evento per il bottone "Dormi"
